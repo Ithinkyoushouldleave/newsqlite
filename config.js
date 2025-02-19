8e2482ccc04e761366f950d01830fd0f944a9d59
anon <153351746+Ithinkyoushouldleave@users.noreply.github.com>
2025-02-18 21:30:23 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const API_BASE_URL = window.location.hostname === "localhost" 
-    ? "http://localhost:8080"  // ✅ Local Dev
-    : "https://testingsqlite.onrender.com";  // ✅ Render Deployment
-
-export default API_BASE_URL;
